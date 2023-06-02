87cb1c212d8c4898832c0231f609500d5cc6f34d
Anirudh r k <arajeshkumar@character.xyz>
2023-06-02 21:50:50 +0530

    basic states and handling

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,15 @@
-function Modal() {
+function Modal(props) {
+  function cancel(){
+    props.onCancel();
+  }
+  function confirm(){
+    props.onConfirm();
+  }
   return (
     <div className="modal">
       <h3>are u sure?</h3>
-      <button className="btn">cancel</button>
-      <button>confirm</button>
+      <button className="btn btn--alt" onClick={cancel}>cancel</button>
+      <button className="btn" onClick={confirm}>confirm</button>
     </div>
   );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,29 @@
-function Todo(props){
-    function deleteHandler(){
-        console.log("clicked");
-    }
-    return (
-        <div className="card">
-          <h2>{props.title}</h2>
-          <button className="btn" onClick={deleteHandler}>delete</button>
-        </div>
-    );
+import { useState } from "react";
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
+function Todo(props) {
+  //returns value and the state chane function
+  const [showModal, setModalOpen] = useState(false);
+
+  function deleteHandler() {
+    setModalOpen(true);
+  }
+
+  function closeHandler(){
+    setModalOpen(false);
+  }
+  return (
+    <div className="card">
+      <h2>{props.title}</h2>
+      <div className="actions">
+        <button className="btn" onClick={deleteHandler}>
+          delete
+        </button>
+      </div>
+      {showModal ? <Modal onCancel={closeHandler} onConfirm={closeHandler}/> : null};
+      {showModal?<Backdrop onClick={closeHandler}/>:null};
+    </div>
+  );
 }
 
 export default Todo;
