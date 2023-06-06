8dda61950c162ab615707c4ca2a244471addcec3
Anirudh r k <arajeshkumar@character.xyz>
2023-06-06 21:38:56 +0530

    complete task added and styling changed

--- src/components/Time.js
+++ src/components/Time.js
@@ -1,11 +1,15 @@
-function Time(props){
-    return(
-        <div className="time">
-            <button className="btn" onClick={props.onIncrease}>+</button>
-            <p>{props.time}</p>
-            <button className="btn" onClick={props.onDecrease}>-</button>
-        </div>
-    )
+function Time(props) {
+  return (
+    <div className="time">
+      <button className="btn" onClick={props.onIncrease}>
+        -
+      </button>
+      <p>{props.time}</p>
+      <button className="btn" onClick={props.onDecrease}>
+        +
+      </button>
+    </div>
+  );
 }
 
 export default Time;
